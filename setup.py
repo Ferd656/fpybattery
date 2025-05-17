dc09069e4696f67a5736a2e86da99371300d44ae
Ferdinand <ferd656@gmail.com>
2025-05-16 18:46:57 -0600

    Update setup.py
    setup configuration

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ferds_batterpy", 
+    name="fpybattery", 
     version="0.1.0",
     packages=find_packages(),
     install_requires=['torch', 'subprocess', 'nvidia-ml-py3'],
@@ -10,7 +10,7 @@ setup(
     description="Ferd's battery of python functions for data science projects",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Ferd656/ferds_batterpy",
+    url="https://github.com/Ferd656/fpybattery",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Public Domain",
